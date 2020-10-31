d826e828075dc306c3fc76e1fa53280dcd14871a
emielnk <emiel.jundy@gmail.com>
2020-10-31 10:03:34 +0700

    this is my first commit python

---
+++ src/test.py
@@ -0,0 +1,12 @@
+def main() :
+    times = input("Input how much time do u want to print Hello World: ")
+    helloWorldForNTimes(times)
+
+def helloWorld():
+    print("Hello World")
+
+def helloWorldForNTimes(times) :
+    for i in range(times) :
+        print("Hello World")
+
+main()
